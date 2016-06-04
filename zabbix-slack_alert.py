f4113e99198830d3ff01f3828bf45fb56f3b7665
Adam Kubica <caffecoder@kaizen-step.com>
2016-06-04 14:15:33 +0200

    A few improvements

--- zabbix-slack_alert.py
+++ zabbix-slack_alert.py
@@ -1,26 +1,28 @@
 #!/usr/bin/env python
 
-# Requirements:
-# pip install requests
-
 import requests
 import sys
 
-slack_user_name = 'alert-bot'
+slack_user_name = 'zabbix'
+
 slack_webhook_url = 'https://hooks.slack.com/services/rest_of_your_webhook_url_part'
 
 if len(sys.argv) != 3:
-    print('Usage: alert-bot.py channel_name "message"')
+    print('Usage:')
+    print(' - zabbix-slack_alert.py "#channel_name" "message"')
+    print(' - zabbix-slack_alert.py "@user_name" "message"')
     sys.exit(1)
 
 alert_data = {
-    "channel": "#%s" % sys.argv[1],
-    "username": slack_user_name,
-    "text": "%s" % sys.argv[2],
+    'channel': "%s" % sys.argv[1],
+    'username': slack_user_name,
+    'text': "%s" % sys.argv[2],
 }
 
+headers_data = { 'User-Agent': 'ZABBIX Alert Script (see: https://github.com/caffecoder/zabbix-slack_alert)' }
+
 requests.packages.urllib3.disable_warnings()
-response = requests.post(slack_webhook_url, json=alert_data)
+response = requests.post(slack_webhook_url, headers=headers_data, json=alert_data)
 
 result = 0 if response.status_code == 200 else 1
 
