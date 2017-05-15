a999ef0f276bed87a08a78ab2ba593a0c106a6a7
Adam Kubica <xcdr@kaizen-step.com>
2017-05-15 22:42:30 +0200

    Rename caffecoder -> xcdr, small refactoring

--- zabbix-slack_alert.py
+++ zabbix-slack_alert.py
@@ -7,11 +7,14 @@ slack_user_name = 'zabbix'
 
 slack_webhook_url = 'https://hooks.slack.com/services/rest_of_your_webhook_url_part'
 
+result = 1
+
 if len(sys.argv) != 3:
     print('Usage:')
     print(' - zabbix-slack_alert.py "#channel_name" "message"')
     print(' - zabbix-slack_alert.py "@user_name" "message"')
-    sys.exit(1)
+
+    sys.exit(result)
 
 alert_data = {
     'channel': "%s" % sys.argv[1],
@@ -19,11 +22,12 @@ alert_data = {
     'text': "%s" % sys.argv[2],
 }
 
-headers_data = { 'User-Agent': 'ZABBIX Alert Script (see: https://github.com/caffecoder/zabbix-slack_alert)' }
+headers_data = { 'User-Agent': 'ZABBIX Alert Script (see: https://github.com/xcdr/zabbix-slack_alert)' }
 
 requests.packages.urllib3.disable_warnings()
 response = requests.post(slack_webhook_url, headers=headers_data, json=alert_data)
 
-result = 0 if response.status_code == 200 else 1
+if response.status_code == 200:
+    result = 0
 
 sys.exit(result)
